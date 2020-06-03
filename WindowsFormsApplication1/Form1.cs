b0b9dd006a51b736dc004dd5839fa32d826888b5
longjiang.he <longjiang.he@NCQLONGJIANG-H>
2020-06-03 09:56:28 +0800

    lab1增加点击事件

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("123");
         }
 
         private void button1_Click(object sender, EventArgs e)
